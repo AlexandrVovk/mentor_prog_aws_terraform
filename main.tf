22c086e6e7a9472f02c21de1dff3a677a35bf10c
Oleksandr <oleksandr_vovk@epam.com>
2021-10-19 19:59:16 +0300

    add vpc gw nat eip

--- main.tf
+++ main.tf
@@ -11,14 +11,64 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "eu-central-1"
+  # region  = "eu-central-1"
+  region = "eu-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-058e6df85cfc7760b"
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   # ami           = "ami-058e6df85cfc7760b"
+#   ami           = "ami-02f5781cba46a5e8a"
+#   instance_type = "t2.micro"
+#
+#   tags = {
+#     Name = "PublicEc2"
+#   }
+# }
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "My-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "privat" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Privat Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "NAT GW"
   }
 }
