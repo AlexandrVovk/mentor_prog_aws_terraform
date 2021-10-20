b1e07c153c1e0bd7351d901a30b4adce8f886a0c
Oleksandr <oleksandr_vovk@epam.com>
2021-10-20 19:49:46 +0300

    add routes and sg and etc

--- main.tf
+++ main.tf
@@ -12,18 +12,10 @@ terraform {
 provider "aws" {
   profile = "default"
   # region  = "eu-central-1"
-  region = "eu-west-2"
+  region = "eu-north-1"
 }
 
-# resource "aws_instance" "app_server" {
-#   # ami           = "ami-058e6df85cfc7760b"
-#   ami           = "ami-02f5781cba46a5e8a"
-#   instance_type = "t2.micro"
-#
-#   tags = {
-#     Name = "PublicEc2"
-#   }
-# }
+############### Public
 
 resource "aws_vpc" "my_vpc" {
   cidr_block       = "10.0.0.0/16"
@@ -31,24 +23,6 @@ resource "aws_vpc" "my_vpc" {
 
   tags = {
     Name = "My-vpc"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Public Subnet"
-  }
-}
-
-resource "aws_subnet" "privat" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "Privat Subnet"
   }
 }
 
@@ -60,15 +34,163 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public_route"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "assoc_pub_sub" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+
+resource "aws_security_group" "public" {
+  name        = "public"
+  description = "ec2 public policy"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "icmp"
+    from_port   = "-1"
+    to_port     = "-1"
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name = "name"
+    values = [
+    "amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  key_name               = "mac-key"
+  vpc_security_group_ids = [aws_security_group.public.id]
+  subnet_id              = aws_subnet.public.id
+
+  tags = {
+    Name = "PublicEc2"
+  }
+}
+
+######################## Privat
+
+
 resource "aws_eip" "eip" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.privat.id
 
   tags = {
     Name = "NAT GW"
+  }
+}
+
+resource "aws_subnet" "privat" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_route_table" "privat_route" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "privat_route"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "assoc_pri_sub" {
+  subnet_id      = aws_subnet.privat.id
+  route_table_id = aws_route_table.privat_route.id
+}
+
+
+resource "aws_security_group" "privat" {
+  name        = "privat"
+  description = "ec2 public policy"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "privat"
+  }
+}
+
+resource "aws_instance" "privat_ec2" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  key_name               = "mac-key"
+  vpc_security_group_ids = [aws_security_group.privat.id]
+  subnet_id              = aws_subnet.privat.id
+
+  tags = {
+    Name = "PrivatEc2"
   }
 }
